cba0d924d387d94b7ed029833091c1e82e11a12b
makovsvetlin <markovsve@gmail.com>
2021-04-09 18:04:50 +0300

    clear a mistake

--- db.js
+++ db.js
@@ -1,25 +1,25 @@
-export const account1 = {
+const account1 = {
   owner: 'Svetlin Markov',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
-export const account2 = {
+const account2 = {
   owner: 'John Doe',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
-export const account3 = {
+const account3 = {
   owner: 'Steven Thomas ',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
-export const account4 = {
+const account4 = {
   owner: 'Sarah Williams',
   movements: [430, 1000, 700, 50, 90, 100, -500],
   interestRate: 1,
